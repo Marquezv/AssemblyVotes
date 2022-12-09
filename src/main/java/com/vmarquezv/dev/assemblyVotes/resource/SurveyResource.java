ebaa8c0d3f539192ec182bd6cab3a67e26ea1e9f
Vinicius Marques <vmarquezv@outlook.com.br>
2022-12-08 22:32:00 -0300

    :sparkles: Survey - Added route findById

--- src/main/java/com/vmarquezv/dev/assemblyVotes/resource/SurveyResource.java
+++ src/main/java/com/vmarquezv/dev/assemblyVotes/resource/SurveyResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,5 +36,10 @@ public class SurveyResource {
 	@GetMapping
 	public ResponseEntity<List<SurveyResponseDTO>> findAll() {
 		return ResponseEntity.ok().body(service.findAll());
+	}
+	
+	@GetMapping(value = ID)
+	public ResponseEntity<SurveyResponseDTO> findById(@PathVariable Long id) {
+		return ResponseEntity.ok().body(service.findById(id).toResponse());
 	}
 }
